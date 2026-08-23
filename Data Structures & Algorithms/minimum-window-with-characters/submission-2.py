dac5c3d139d9e5e893e4d184eb0a54e36813273b
Isha Kanade <66054580+ishakanade@users.noreply.github.com>
2026-08-23 23:31:46 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # similar to Permutation in String
+        count = {}
+        window = {}
+        for i in t:
+            count[i] = 1 + count.get(i, 0)
+        l = 0
+        matches = 0
+        result=""
+        min_len = float('inf')
+        for r in range(len(s)):
+            window[s[r]] = window.get(s[r], 0) + 1
+            if s[r] in count and count[s[r]] == window[s[r]]:
+                matches+=1
+            while matches == len(count):
+                if (r - l + 1) < min_len:
+                    min_len = r - l + 1
+                    result = s[l:r+1]
+                window[s[l]]-=1
+                if s[l] in count and count[s[l]] > window[s[l]]:
+                    matches-=1
+                l+=1
+        return result
